9e36ec9533f72c9a7a7438bd95a1942ba626e445
GuillaumeDery98 <dery.guillaume98@outlook.fr>
2022-10-19 13:31:36 +0200

    feat: complete user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,33 +12,46 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
+    public $table = 'users';
+
+    public $fillable = [
         'name',
         'email',
+        'email_verified_at',
         'password',
+        'address',
+        'phone',
+        'biography',
+        'picture',
+        'cv',
+        'remember_token'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
+        'name' => 'string',
+        'email' => 'string',
         'email_verified_at' => 'datetime',
+        'password' => 'string',
+        'address' => 'string',
+        'phone' => 'string',
+        'biography' => 'string',
+        'picture' => 'string',
+        'cv' => 'string',
+        'remember_token' => 'string'
+    ];
+
+    public static $rules = [
+        'name' => 'required|string',
+        'email' => 'required|string',
+        'email_verified_at' => 'nullable',
+        'password' => 'required|string',
+        'address' => 'required|string',
+        'phone' => 'required|string',
+        'biography' => 'required|string',
+        'picture' => 'required|string',
+        'cv' => 'required|string',
+        'remember_token' => 'nullable|string',
+        'created_at' => 'nullable',
+        'updated_at' => 'nullable'
     ];
 }
